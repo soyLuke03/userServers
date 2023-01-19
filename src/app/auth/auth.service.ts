73da26532676b29c707f36f919a0e2b8a7289c79
joadelvia <joadelvia@gmail.com>
2023-01-19 13:05:31 +0100

    Login con jwt parcial

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { of, Observable, switchMap } from 'rxjs';
+import { of, Observable, switchMap, catchError } from 'rxjs';
 import { UsersService } from '../users/services/users.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthResponse } from '../servers/interfaces/token.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private userService: UsersService) { }
+  url: string = 'http://localhost:8000/auth/login';
   loggedIn = false;
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  constructor(private userService: UsersService, private http:HttpClient) { }
+  
  
   isAuthenticated() {
     //Recuperamos la clave authenticated de localStorage
@@ -25,19 +31,30 @@ export class AuthService {
  
   login(email: string, password: string):Observable<boolean>{
     //Recuperamos el usuario y comprobamos que la contraseña sea correcta
-   return this.userService.getUserByEmail(email)
-    .pipe( switchMap((user=> {
-      if (user.length && user[0].password===password){
-        localStorage.setItem('authenticated', 'true');
-        localStorage.setItem('rol',user[0].rol)
-        return of(true)
-      }
-      else{
-        localStorage.setItem('authenticated', 'false');
-        return of(false)
-      }
-    })))
+  //  return this.userService.getUserByEmail(email)
+  //   .pipe( switchMap((user=> {
+  //     if (user.length && user[0].password===password){
+  //       localStorage.setItem('authenticated', 'true');
+  //       localStorage.setItem('rol',user[0].rol)
+  //       return of(true)
+  //     }
+  //     else{
+  //       localStorage.setItem('authenticated', 'false');
+  //       return of(false)
+  //     }
+  //   })))
+  
     
+  return this.http.post<AuthResponse>(this.url, {email, password},this.httpOptions)
+    .pipe( switchMap(token => {
+      localStorage.setItem('token', token.access_token);
+      return of(true);
+    }),catchError(error => {
+      localStorage.removeItem('token');
+      return of(false);
+    })
+
+    )
 
     // .subscribe({
     //   next: (resp)=> {
@@ -61,7 +78,7 @@ export class AuthService {
  
   logout() {
     //Cambiamos el valor de la clave authenticated a false en localStorage
-    localStorage.setItem('authenticated', 'false')
-    
+    localStorage.setItem('authenticated', 'false');
+    localStorage.removeItem('rol'); 
   }
 }
